9c2aa20094bbb31def0fc3326fb467368cc23cc3
Tim Eubank <timothy.eubank@gmail.com>
2017-09-01 01:01:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  prime = true
+  if num < 2
+    prime = false
+  end
+  counter = 2
+  while counter < num
+    if num % counter == 0
+      prime = false
+    end
+    counter += 1
+  end
+  prime
+end
